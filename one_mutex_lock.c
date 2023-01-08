e7367352f433aa8478324b0dcca98d17604b1a66
Nuvindu <nuvidu.18@cse.mrt.ac.lk>
2023-01-08 09:32:50 +0530

    Improve execution time in linked list one mutex

--- one_mutex_lock.c
+++ one_mutex_lock.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <sys/time.h>
 #include <math.h>
 #include <pthread.h>
 
 #define MAX_VALUE 65535
 #define NUM_THREADS 1
+int *operationsArray;
+int *dataResults;
+unsigned bit;
 
 // Define a node structure
 typedef struct node {
@@ -15,17 +19,15 @@ typedef struct node {
 
 struct thread_args
 {
-    long memberOps;
-    long insertOps;
-    long deleteOps;
+    unsigned short threadId;
+    long threadOperations;
+    long threadCount;
     node* head;
 };
 
-// Function prototypes
-int Member(node* head, int data);
-void Insert(node** head, int data);
-void Delete(node** head, int data);
-void print_list(node* head);
+int Member(node* head_p, int data);
+int Insert(node** head_pp, int data);
+int Delete(node** head_pp, int data);
 pthread_mutex_t mutex_lock;
 pthread_t *threadHandles;
 int memberOperations;
@@ -34,52 +36,71 @@ int deleteOperations;
 
 void *threadOperation(void* thread_args) {
     struct thread_args *args = (struct thread_args *)thread_args;
-    long memberOps = args->memberOps;
-    long insertOps = args->insertOps;
-    long deleteOps = args->deleteOps;
+    long threadOperations = args->threadOperations;
     node* head = args->head;
-    while (memberOps + insertOps + deleteOps > 0) {
-        int op = rand() % 3;
-        int data = rand() % 10000;
-        switch (op) {
+    unsigned short threadId = args->threadId;
+    long threadCount = args->threadCount;
+
+    while(threadId < threadOperations) {
+        int data = dataResults[threadId];
+        if (operationsArray[threadId] == 0)
+        {   
+            pthread_mutex_lock(&mutex_lock);
+            Member(head,data);
+            pthread_mutex_unlock(&mutex_lock);
+        }
+        else if (operationsArray[threadId] == 1)
+        {   
+            pthread_mutex_lock(&mutex_lock);
+            Insert(&head,data);
+            pthread_mutex_unlock(&mutex_lock);
+        }
+        else if (operationsArray[threadId] == 2)
+        {
+            pthread_mutex_lock(&mutex_lock);
+            Delete(&head,data);
+            pthread_mutex_unlock(&mutex_lock);
+        }
+        threadId += threadCount;
+    }
+}
+
+void setOrderOfOperations(long memberOperations,long insertOperations,long deleteOperations, int* operationsArray) {
+    int i=0;
+    while (memberOperations + insertOperations + deleteOperations > 0) {
+        int operationType = rand() % 3;
+        switch (operationType) {
             case 0:
-                if (memberOps > 0) {
-                    pthread_mutex_lock(&mutex_lock);
-                    Member(head, data);
-                    pthread_mutex_unlock(&mutex_lock);
-                    memberOps --;
+                if (memberOperations > 0) {
+                    operationsArray[i] = 0;
+                    memberOperations--;
+                    i++;
                 }
                 break;
             case 1:
-                if (insertOps > 0) {
-                    pthread_mutex_lock(&mutex_lock);
-                    Insert(&head, data);
-                    pthread_mutex_unlock(&mutex_lock);
-                    insertOps--;
+                if (insertOperations > 0) {
+                    operationsArray[i] = 1;
+                    insertOperations--;
+                    i++;
                 }
                 break;
             case 2:
-                if (deleteOps > 0) {
-                    pthread_mutex_lock(&mutex_lock);
-                    Delete(&head, data);
-                    pthread_mutex_unlock(&mutex_lock);
-                    deleteOps--;
+                if (deleteOperations > 0) {
+                    operationsArray[i] = 2;
+                    deleteOperations--;
+                    i++;
                 }
                 break;
-        }
-    }
+            }
+    }    
 }
 
 int main() {
     int num_samples=97;
-    float *timeResults = malloc(sizeof(int) * num_samples);
-    int caseNumber = 0;
-    while (caseNumber < 3) {
-        for (int r = 0; r < num_samples; r++)
+    for (int caseNumber = 0; caseNumber < 3; caseNumber++) {
+        float *timeResults = malloc(sizeof(int) * num_samples);
+        for (int sample = 0; sample < num_samples; sample++)
         {
-        // Seed the random number generator
-            srand(time(0));
-
             // Create the head of the list
             node* head = NULL;
 
@@ -93,7 +114,12 @@ int main() {
             // Perform m random operations on the list
             int m = 10000;
 
-            
+            dataResults = malloc(sizeof(int) * m);
+            for (int i = 0; i < m; i++) {
+                int data = rand() % MAX_VALUE;
+                dataResults[i]=data;
+            }
+
             switch (caseNumber) {
                 case 0:
                     memberOperations = m * 0.99;
@@ -108,19 +134,25 @@ int main() {
                     insertOperations = m * 0.25;
                     deleteOperations = m * 0.25;
             }
-            clock_t start, end;
-            double execution_time;
-            start = clock();
+            operationsArray = malloc(sizeof(int) * m);
+            setOrderOfOperations(memberOperations, insertOperations, deleteOperations, operationsArray);
 
             int threadCount = 8;
-            pthread_mutex_init(&mutex_lock, NULL);
+            int threadOperations = m / threadCount;
+            clock_t start, end;
+            double execution_time;
             threadHandles = malloc(threadCount * sizeof(pthread_t));
+
+
+            start = clock();           
+            pthread_mutex_init(&mutex_lock, NULL);
+            
             for (int t = 0; t < threadCount; t++) {
                 struct thread_args *thread_args_struct = malloc(sizeof(struct thread_args));
-                thread_args_struct->memberOps = memberOperations / threadCount;
-                thread_args_struct->insertOps = insertOperations / threadCount;
-                thread_args_struct->deleteOps = deleteOperations / threadCount;
+                thread_args_struct->threadOperations = threadOperations;
                 thread_args_struct->head = head;
+                thread_args_struct->threadId = t;
+                thread_args_struct->threadCount = threadCount;
                 pthread_create(&threadHandles[t], NULL, threadOperation, (void *)thread_args_struct);
             }
 
@@ -132,12 +164,13 @@ int main() {
             pthread_mutex_destroy(&mutex_lock);
 
             end = clock();
+
+            
             execution_time = ((double)(end - start)) / CLOCKS_PER_SEC *1000;
             // printf("execution_time is %f\n", execution_time);
-            timeResults[r]=execution_time;
+            timeResults[sample]=execution_time;
         }
-        // Print the final list
-        // print_list(head);
+ 
         double sum;
         for (int t = 0; t < num_samples; t++)
         {
@@ -150,12 +183,9 @@ int main() {
             sd += pow(timeResults[q] - mean, 2);
         sd = sqrt(sd / num_samples);
         printf("Standard Deviation is %f\n", sd);
-        caseNumber++;
     }
     return 0;
 }
-
-
 
 // Check if a value is in the list
 int Member(node* head, int data) {
@@ -170,19 +200,20 @@ int Member(node* head, int data) {
 }
 
 // Insert a new value into the list
-void Insert(node** head, int data) {
+int Insert(node** head, int data) {
     if (Member(*head, data)) {
-        return;
+        return 0;
     }
 
     node* new_node = (node*) malloc(sizeof(node));
     new_node->data = data;
     new_node->next = *head;
     *head = new_node;
+    return 1;
 }
 
 // Delete a value from the list
-void Delete(node** head, int data) {
+int Delete(node** head, int data) {
     node* current = *head;
     node* prev = NULL;
     while (current != NULL) {
@@ -193,20 +224,10 @@ void Delete(node** head, int data) {
                 prev->next = current->next;
             }
             free(current);
-            return;
+            return 1;
         }
         prev = current;
         current = current->next;
     }
-}
-
-// Print the contents of the list
-void print_list(node* head) {
-    // printf("List: ");
-    node* current = head;
-    while (current != NULL) {
-        // printf("%d ", current->data);
-        current = current->next;
-    }
-    // printf("\n");
+    return 0;
 }
